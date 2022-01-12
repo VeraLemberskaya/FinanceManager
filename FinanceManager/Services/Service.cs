b137165f54d8123110e7fe90219b5c6f04b90446
Vera Lemberskaya <veralemberskaa@gmail.com>
2022-01-12 17:10:45 +0300

    database ensuring created

--- FinanceManager/Services/Service.cs
+++ FinanceManager/Services/Service.cs
@@ -79,6 +79,7 @@ namespace FinanceManager.Services
         private Service()
         {
             _db = new ApplicationContext();
+            _db.Database.EnsureCreated();
             _defaultCurrency = Currency[Setting.Default.index];            
         }
 
